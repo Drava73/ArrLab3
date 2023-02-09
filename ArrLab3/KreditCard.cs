622c6ce1caa8e6c9ef61e2f5bba931c795ad0a9e
My Name <kiykobogdan@gmail.com>
2023-02-09 14:41:53 +0200

    Add project files.

---
+++ ArrLab3/KreditCard.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrLab3
+{
+    public class KreditCard
+    {
+        public int numbercard;
+        public string Name;
+        public int CVC;
+        public string timeofend;
+        
+        
+        public int NameCard
+        {
+            get { return numbercard; }
+
+            set { numbercard = value; }
+        }
+        public int CCVVCC
+        {
+            get { return CVC; }
+
+            set { CVC = value; }
+        }
+        public String NamePerson
+        {
+            get { return Name; }
+
+            set { Name = value; }
+        }
+        public string Endcard
+        {
+            get { return timeofend; }
+
+            set { timeofend = value; }
+        }
+        public KreditCard() {
+            Random r = new Random();
+            numbercard = r.Next(10000000,99999999);
+            Name = "None";
+            CVC = r.Next(100,999);
+            timeofend = "None";
+        }
+        public KreditCard(int numbercard, string Name, int CVC, string timeofend)
+        {
+            this.numbercard = numbercard;
+            this.Name = Name;
+            this.CVC = CVC;
+            this.timeofend = timeofend;
+
+        }
+        public override string ToString()
+        {
+            return $"Card:{numbercard}\nName:{Name}\nCVC:{CVC}\nEnd card:{timeofend}";
+        }
+
+    }
+}
+
---
+++ ArrLab3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrLab3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            KreditCard card1 = new KreditCard();
+            try {
+                Console.Write("Введите имя владельца карты:");
+                card1.Name = Console.ReadLine();
+                if(card1.Name == "" || card1.Name == "Name" || card1.Name =="admin") {
+                    throw new Exception("Введите имя владельца карты!");
+                }
+            }
+            catch(Exception ex) {
+                Console.WriteLine(ex.Message);
+
+            }
+            Console.Write(card1);
+            card1.Endcard = "24/12";
+            Console.ReadLine();
+        }
+    }
+}
